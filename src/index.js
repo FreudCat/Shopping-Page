cc794351ec53cb7f8335af505ea1dc4fb82e2160
= <anguyen031@gmail.com>
2022-02-03 19:44:19 -0800

    Add functionality to add to cart button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 console.log("index.js ran");
 const shoeDivArray = document.querySelectorAll(".thumbnail-shoe-div");
 const shoeArray = document.querySelectorAll(".thumbnail-shoe");
@@ -25,3 +24,58 @@ function highlightShoe(activeShoeIndex) {
   }
 }
 
+
+let currentCartAmount = parseInt(document.querySelector(".cart-amount-holder").innerHTML);
+let amountToAdd = parseInt(document.querySelector(".number-holder").innerHTML);
+
+
+document.querySelector(".minus").addEventListener("click", function() {
+  updateNumber(amountToAdd-=1);
+})
+document.querySelector(".plus").addEventListener("click", function() {
+  updateNumber(amountToAdd+=1);
+})
+document.querySelector(".add-cart-button").addEventListener("click", function() {
+  updateCart(amountToAdd);
+})
+
+function updateNumber(value) {
+  console.log(value);
+  if (value < 0) {
+    console.log("User clicked (-) button when value is already 0");
+    amountToAdd=0;
+  }
+  else {
+    document.querySelector(".number-holder").innerHTML=value;
+  }
+}
+
+function updateCart(value) {
+  if (value === 0) {
+    document.querySelector(".cart-amount-holder").style.display="none";
+    console.log("cart updated with value of 0 and orange bubble removed");
+  } else {
+    document.querySelector(".cart-amount-holder").style.display="flex";
+    document.querySelector(".cart-amount-holder").innerHTML = value;
+    console.log("update cart val with new amount and bubble is visible");
+  }
+} 
+
+
+
+if(window.innerWidth < 700){
+  console.log("innderwidth");
+  document.querySelector(".main-image").removeAttribute("data-bs-toggle");
+}
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 700) {
+    document.querySelector(".main-image").setAttribute("data-bs-toggle", "modal");
+  } else {
+    document.querySelector(".main-image").removeAttribute("data-bs-toggle");
+}
+});
+
+document.querySelector(".main-image").addEventListener("click", function() {
+  document.querySelector(".modal-image").src=document.querySelector(".main-image").src;
+
+  })
